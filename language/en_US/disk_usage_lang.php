ed03452c477fd770b3751bf2d510d7152b61cc09
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-04-19 00:36:09 +0000

    - Harmonized app description

--- language/en_US/disk_usage_lang.php
+++ language/en_US/disk_usage_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['disk_usage_app_name'] = 'Disk Usage';
-$lang['disk_usage_app_description'] = 'Displays your system hard disk usage.';
+$lang['disk_usage_app_description'] = 'The Disk Usage app displays your system hard disk usage and provides a clickable drill down report.';
 $lang['disk_usage_app_tooltip'] = 'Disk usage information is stored in a database for fast retrieval and updated every 24 hours.  The graph you are viewing does not display real-time information.  Click on the areas of the graph to zoom into folders and click on \'cd..\' to return to the previous folder.';
 $lang['disk_usage_coordinate_invalid'] = 'Coordinate is invalid.';
 $lang['disk_usage_path_invalid'] = 'Path is invalid.';
