63367e74d0c4fb36adb10d3434da233d9ec452a7
pcbaldwin <peter@egloo.ca>
2017-02-24 12:51:54 -0500

    Prepped for translation system

--- language/en_US/disk_usage_lang.php
+++ language/en_US/disk_usage_lang.php
@@ -2,7 +2,7 @@
 
 $lang['disk_usage_app_description'] = 'The Disk Usage Report app displays your system hard disk usage and provides a clickable drill down report.';
 $lang['disk_usage_app_name'] = 'Disk Usage Report';
-$lang['disk_usage_app_tooltip'] = 'Disk usage information is stored in a database for fast retrieval and updated every 24 hours.  The graph you are viewing does not display real-time information.  Click on the areas of the graph to zoom into folders and click on \'cd..\' to return to the previous folder.';
+$lang['disk_usage_app_tooltip'] = 'Disk usage information is stored in a database for fast retrieval and updated every 24 hours.  The graph you are viewing does not display real-time information.  Click on the areas of the graph to zoom into folders and click on cd.. to return to the previous folder.';
 $lang['disk_usage_coordinate_invalid'] = 'Co-ordinate is invalid.';
 $lang['disk_usage_path_invalid'] = 'Path is invalid.';
 $lang['disk_usage_updating_disk_usage_information'] = 'Updating disk usage information.  This can take several minutes.';
