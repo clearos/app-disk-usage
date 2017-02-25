e621658a582a4be4bc7523df810407768d79d218
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:33:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disko naudojimo ataskaita programa rodo savo sistemos kietojo disko naudojimą ir suteikia spausti išskleiskite ataskaitą.';
+$lang['disk_usage_app_name'] = 'Disko naudojimo ataskaita';
+$lang['disk_usage_app_tooltip'] = 'Disko naudojimas informacija yra saugoma būtų greito paieškos duomenų bazėje ir atnaujinama kas 24 valandas. Grafikas žiūrite nerodo realaus laiko informaciją. Spauskite ant diagramoje sričių pateksite į aplankus ir paspauskite ant cd .. Norėdami grįžti į ankstesnį aplanką.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinuoja negalioja.';
+$lang['disk_usage_path_invalid'] = 'Kelias yra neteisinga.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Atnaujinama disko naudojimo informaciją. Tai gali užtrukti keletą minučių.';
