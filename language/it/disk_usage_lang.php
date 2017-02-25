b0fa0553d19e716dfa2ae7b61fb4bae103930983
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:31:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Il Rapporto Disk Usage applicazione visualizza il sistema utilizzo del disco rigido e fornisce un rapporto di drill-down cliccabile.';
+$lang['disk_usage_app_name'] = 'Rapporto Disk Usage';
+$lang['disk_usage_app_tooltip'] = 'Informazioni utilizzo del disco viene memorizzato in un database per il recupero veloce e aggiornato ogni 24 ore. Il grafico che si sta leggendo non visualizzare informazioni in tempo reale. Clicca sulle aree del grafico per ingrandire in cartelle e cliccare su cd .. per tornare alla cartella precedente.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-ordinate non è valido.';
+$lang['disk_usage_path_invalid'] = 'Percorso non è valido.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Aggiornamento delle informazioni di utilizzo del disco. Questo può richiedere alcuni minuti.';
