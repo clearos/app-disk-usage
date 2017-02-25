202d52ae478b04495dfd241d54ac9bb92edb9073
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:28:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'U Disk Usage Report aplikaciju prikazuje sistem korištenje hard disk i pruža kliknuti drill down izvještaj.';
+$lang['disk_usage_app_name'] = 'Disk Usage Report';
+$lang['disk_usage_app_tooltip'] = 'informacije o upotrebi Disk se čuvaju u bazi podataka za brzo pronalaženje i ažuriraju svakih 24 sata. Na grafikonu gledate ne prikazuje informacije u realnom vremenu. Kliknite na područja grafikona da biste zumirali u foldere i kliknite na cd .. da se vrati na prethodni folder.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordiniraju je nevažeći.';
+$lang['disk_usage_path_invalid'] = 'Path je nevažeći.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Ažuriranje informacija o upotrebi diska. Ovo može potrajati nekoliko minuta.';
