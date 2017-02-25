23fdbb733061dd34a513e89f95c903f53aa94fac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:33:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Whakaatu te taupānga Report Raihana Kōpae koutou whakamahinga kōpae mārō pūnaha me whakarato te clickable wiri iho pūrongo.';
+$lang['disk_usage_app_name'] = 'Pūrongo Mahinga Kōpae';
+$lang['disk_usage_app_tooltip'] = 'rongoa mōhiohio whakamahinga kōpae i roto i te pātengi raraunga mō te tiki nohopuku me te whakahoutia nga 24 haora. Te kauwhata e tiro koutou e kore e whakaatu mōhiohio wā-tūturu. Pāwhiritia ki te wāhi o te kauwhata ki te topa ki ngā kōpaki, ka pāwhiri i runga i cd .. ki te hoki ki te kōpaki o mua.';
+$lang['disk_usage_coordinate_invalid'] = 'He muhu Co-whakarite.';
+$lang['disk_usage_path_invalid'] = 'He muhu Ara.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Whakahōu mōhiohio whakamahinga kōpae. Ka taea e te tango i tēnei e rave rahi meneti.';
