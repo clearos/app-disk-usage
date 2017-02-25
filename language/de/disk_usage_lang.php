593d19e3ca4f15a2af40a11304a61e2655023699
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:30:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Die Disk Usage Bericht App zeigt der Systemfestplattennutzung und bietet einen klickbaren Drill-Down-Bericht.';
+$lang['disk_usage_app_name'] = 'Disk Usage Bericht';
+$lang['disk_usage_app_tooltip'] = 'Disk-Nutzungsinformationen werden in einer Datenbank für den schnellen Abruf und aktualisiert alle 24 Stunden gespeichert. Die Grafik, die Sie sehen keine Informationen in Echtzeit angezeigt werden. Klicken Sie auf die Bereiche des Graphen in Ordnern zu vergrößern, und klicken Sie auf cd .. zum vorherigen Ordner zurückzukehren.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinaten ist ungültig.';
+$lang['disk_usage_path_invalid'] = 'Pfad ist ungültig.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Aktualisieren der Datenträgerverwendung Informationen. Dies kann einige Minuten dauern.';
