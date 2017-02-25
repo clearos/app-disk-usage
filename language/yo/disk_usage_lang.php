4a4579fd7a4322d4033deb4281393b9dee8d0608
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:39:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Awọn Disk Lilo Iroyin app han rẹ eto lile disk lilo ati ki o pese a clickable lu si isalẹ Iroyin.';
+$lang['disk_usage_app_name'] = 'Disk Lilo Iroyin';
+$lang['disk_usage_app_tooltip'] = 'Disk lilo alaye ti o ti fipamọ ni a database fun sare igbapada ati ki o imudojuiwọn gbogbo 24 wakati. Awonya ti o ba nwo ko ni han gidi-akoko alaye. Tẹ lori awọn agbegbe ti awọn awonya lati sun sinu folda ki o si tẹ lori CD .. lati pada si awọn ti tẹlẹ folda.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-ordinate ni invalid.';
+$lang['disk_usage_path_invalid'] = 'Ona ti wa ni invalid.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Nmu disk lilo alaye. Eleyi le ya awọn orisirisi awọn iṣẹju.';
