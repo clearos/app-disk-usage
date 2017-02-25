5669141119483a1d98cc59fa5c71f9ad9704de55
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:27:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disko-erabileraren txostena Aplikazioa zure sistema disko gogorraren erabilera bistaratzen eta klikagarri zulatzeko behera txosten bat eskaintzen du.';
+$lang['disk_usage_app_name'] = 'Disko-erabileraren txostena';
+$lang['disk_usage_app_tooltip'] = 'Disko erabilera informazioaren berreskuratze azkarra datu-base batean gordetzen dira, eta 24 orduro eguneratzen. Grafikoan ikusten ari zaren ez du denbora errealean informazioa erakusteko. Egin klik grafikoan arloei buruzko karpetak hurbiltzea eta sakatu cd on .. to aurreko karpetan itzultzeko.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinatzea baliogabea da.';
+$lang['disk_usage_path_invalid'] = 'Bide baliogabea da.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Disko erabileraren informazioa eguneratzeko. Hau zenbait minutu luza daiteke.';
