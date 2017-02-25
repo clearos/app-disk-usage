a93211d361c634b9086fdb18c81fc379699a0bbf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:29:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Levyn Käyttöraportti sovellus näyttää järjestelmän kiintolevyn käyttöä ja tarjoaa napsautettava porautua raportti.';
+$lang['disk_usage_app_name'] = 'Levyn Käyttöraportti';
+$lang['disk_usage_app_tooltip'] = 'Levynkäyttöä tieto tallennetaan tietokantaan noutaa nopeasti ja päivitetään 24 tunnin välein. Käyrä katselet ei näytä reaaliaikaista tietoa. Klikkaa alueita lähentää kaavion kansioihin ja klikkaa cd .. palata edelliseen kansioon.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinoivat ei kelpaa.';
+$lang['disk_usage_path_invalid'] = 'Polku ei kelpaa.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Päivittäminen levyn käyttötietoja. Tämä voi kestää useita minuutteja.';
