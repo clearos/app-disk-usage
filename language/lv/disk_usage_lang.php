563a7b7297833ee389e6a1eeff2e086b650e82c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:33:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disk Usage ziņojumā app parāda jūsu sistēmas cietā diska izmantošana un nodrošina klikšķināmos urbt leju ziņojumu.';
+$lang['disk_usage_app_name'] = 'Disk Usage pārskats';
+$lang['disk_usage_app_tooltip'] = 'Diska izmantošana informācija tiek glabāta datu bāzē ātri izguves un atjaunina ik pēc 24 stundām. Grafikā skatāt nerāda reālā laika informāciju. Uzklikšķiniet uz jomām, grafika, lai tuvinātu mapēs un noklikšķiniet uz cd .. lai atgrieztos uz iepriekšējo mapi.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinē ir nederīgs.';
+$lang['disk_usage_path_invalid'] = 'Ceļš ir nederīgs.';
+$lang['disk_usage_updating_disk_usage_information'] = 'diska izmantošana informācijas atjaunināšanu. Tas var aizņemt vairākas minūtes.';
