2b6964d3eb3e59598f03399b138abfbc4be75587
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:34:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Den Disk Usage Rapporter app viser systemet hardbruk disken og gir en klikkbar bore ned rapporten.';
+$lang['disk_usage_app_name'] = 'Disk Usage Rapporter';
+$lang['disk_usage_app_tooltip'] = 'Disk bruk informasjonen blir lagret i en database for rask gjenfinning og oppdateres hver 24. time. Grafen du viser ikke viser sanntidsinformasjon. Klikk på de områdene av grafen for å zoome inn mapper og klikk på cd .. for å gå tilbake til forrige mappe.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinere er ugyldig.';
+$lang['disk_usage_path_invalid'] = 'Path er ugyldig.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Oppdatering disk bruk informasjon. Dette kan ta flere minutter.';
