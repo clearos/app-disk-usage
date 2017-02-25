c568974b65398728a7d84eb3c6a767eb183f1d91
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:33:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Ny kapila Usage Report fampiharana mampiseho ny rafitra fampiasana kapila mafy sy manome ny clickable fandavahana teo tatitra.';
+$lang['disk_usage_app_name'] = 'Kapila Usage Report';
+$lang['disk_usage_app_tooltip'] = 'Kapila fampiasana fanazavana dia voatahiry ao amin&#39;ny tahiry ho an&#39;ny fifadian-kanina retrieval sy nohavaozina isaky ny 24 ora. Ny sary ianao mijery tsy maneho tena-fampahalalana momba ny fotoana. Tsindrio eo amin&#39;ny sehatry ny ny sary hanao zoom ho laha-tahiry ary tsindrio eo amin&#39;ny CD .. mba hiverina any amin&#39;ny teo aloha fampirimana.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-ordinate dia tsy manan-kery.';
+$lang['disk_usage_path_invalid'] = 'Path dia tsy manan-kery.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Fanavaozana fampiasana kapila vaovao. Izany afaka minitra maromaro.';
