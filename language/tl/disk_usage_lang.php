c41836d0c9a485609e165ee4e8820e25e83c0135
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:37:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Ipinapakita ng Disk Paggamit Isumbong ang app ang iyong system hard disk paggamit at nagbibigay ng isang naki-click na mag-drill down ulat.';
+$lang['disk_usage_app_name'] = 'Disk Paggamit Isumbong ang';
+$lang['disk_usage_app_tooltip'] = 'Paggamit ng disk impormasyon ay naka-imbak sa isang database para sa mabilis na pagkuha at na-update sa bawat 24 na oras. Ang graph tinitingnan mo ay hindi ipakita ang real-time na impormasyon. Mag-click sa mga lugar ng graph upang mag-zoom sa mga folder at i-click cd .. upang bumalik sa nakaraang folder.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-ordinate ay hindi wasto.';
+$lang['disk_usage_path_invalid'] = 'Path ay hindi wasto.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Ina-update ang impormasyon sa disk paggamit. Ito ay maaaring tumagal ng ilang minuto.';
