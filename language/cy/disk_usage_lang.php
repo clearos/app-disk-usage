ca29f2a27d47e84c3980dc129cdf6254cc290b2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:38:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Mae&#39;r app Adroddiad Defnydd Choeten dangos eich system defnydd disg galed ac yn darparu dyrchu adroddiad cliciadwy.';
+$lang['disk_usage_app_name'] = 'Adroddiad Defnydd Choeten';
+$lang['disk_usage_app_tooltip'] = 'gwybodaeth defnydd disg yn cael ei storio mewn cronfa ddata ar gyfer adfer yn gyflym ac yn diweddaru bob 24 awr. Mae&#39;r graff ydych yn edrych arni ddim yn arddangos gwybodaeth amser real. Cliciwch ar y rhannau o&#39;r graff i chwyddo i mewn i ffolderi a chliciwch ar cd .. i ddychwelyd at &#39;r folder flaenorol.';
+$lang['disk_usage_coordinate_invalid'] = 'Cydlynu yn annilys.';
+$lang['disk_usage_path_invalid'] = 'Llwybr yn annilys.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Diweddaru gwybodaeth defnydd disg. Gall hyn gymryd sawl munud.';
