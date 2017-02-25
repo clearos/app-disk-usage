fe5dcba369b8376b87683ab378fc6054c8f480e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:30:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'The Disk Anfani Report app nuna tsarin wuya faifai mai cutarwa da kuma samar da wani clickable rawar soja saukar rahoton.';
+$lang['disk_usage_app_name'] = 'Disk Anfani Report';
+$lang['disk_usage_app_tooltip'] = 'Disk amfani bayanai da aka adana a cikin wani database for azumi gyara da updated kowace 24 hours. A jadawali da kake ganin ba nuna hakikanin-lokaci bayani. Click a kan yankunan da jadawali don zuƙowa ciki a cikin manyan fayiloli da kuma danna kan cd .. komawa baya fayil.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-ordinate ba daidai ba ne.';
+$lang['disk_usage_path_invalid'] = 'Path ba daidai ba ne.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Ana ɗaukaka faifai mai cutarwa bayani. Wannan na iya daukar da dama da minti.';
