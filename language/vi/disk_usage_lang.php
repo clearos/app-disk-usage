5f6b5d04f4320b95c8ab862df063040ecf0461e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:38:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Ứng dụng Báo cáo Disk Usage hiển thị hệ thống sử dụng đĩa cứng của bạn và cung cấp một nhấp được khoan xuống báo cáo.';
+$lang['disk_usage_app_name'] = 'Báo cáo Disk Usage';
+$lang['disk_usage_app_tooltip'] = 'thông tin sử dụng đĩa được lưu trữ trong một cơ sở dữ liệu để thu hồi nhanh chóng và cập nhật mỗi 24 giờ. Các đồ thị, bạn đang xem không hiển thị thông tin thời gian thực. Nhấp chuột vào các lĩnh vực biểu đồ để phóng to vào các thư mục và click vào cd .. để trở về thư mục trước đó.';
+$lang['disk_usage_coordinate_invalid'] = 'Phối hợp là không hợp lệ.';
+$lang['disk_usage_path_invalid'] = 'Đường dẫn không hợp lệ.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Cập nhật thông tin sử dụng đĩa. Điều này có thể mất vài phút.';
