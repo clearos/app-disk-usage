cf6a8199c2d4c024b6dbced3161813c794bdfbdd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:38:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disk Kullanım Raporu uygulaması sistem sabit disk kullanımını görüntüler ve tıklanabilir bir detaya rapor sunar.';
+$lang['disk_usage_app_name'] = 'Disk Kullanımı Raporu';
+$lang['disk_usage_app_tooltip'] = 'Disk kullanım bilgileri her 24 saatte hızlı erişim için bir veritabanında saklanır ve güncellenmektedir. Gerçek zamanlı bilgi görüntülemek vermez incelemekte grafiği. klasörler halinde Büyütmek ve önceki klasöre geri dönmek için .. cd tıklayın grafiğin alanlar üzerine tıklayın.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordine geçersiz.';
+$lang['disk_usage_path_invalid'] = 'Yol geçersiz.';
+$lang['disk_usage_updating_disk_usage_information'] = 'disk kullanımını bilgileri güncelleniyor. Bu işlem birkaç dakika sürebilir.';
