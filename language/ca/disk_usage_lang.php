48c9d0741eb46bbea86ca0754fe3d404ab2d4e80
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:28:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'L&#39;ús de disc Informe d&#39;aplicació mostra l&#39;ús del disc dur del sistema i proporciona un informe detallat pot fer clic.';
+$lang['disk_usage_app_name'] = 'Informe d&#39;ús de disc';
+$lang['disk_usage_app_tooltip'] = 'informació d&#39;ús del disc s&#39;emmagatzema en una base de dades per a una ràpida recuperació i s&#39;actualitza cada 24 hores. El gràfic que s&#39;està veient no mostra informació en temps real. Feu clic a les àrees de la gràfica per fer zoom en les carpetes i feu clic en cd .. per tornar a la carpeta anterior.';
+$lang['disk_usage_coordinate_invalid'] = 'Coordenada no és vàlid.';
+$lang['disk_usage_path_invalid'] = 'Ruta no és vàlida.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Actualitzar la informació de l&#39;ús del disc. Això pot trigar diversos minuts.';
