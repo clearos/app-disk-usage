fc73e7a9c72e25b4d940defbc8916bb2879ce479
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:32:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = '디스크 사용량 보고서 응용 프로그램은 시스템의 하드 디스크 사용량을 표시하고 클릭 할 수있는 드릴 다운 보고서를 제공합니다.';
+$lang['disk_usage_app_name'] = '디스크 사용 보고서';
+$lang['disk_usage_app_tooltip'] = '디스크 사용 정보를 24 시간마다 고속 검색을 위해 데이터베이스에 저장되고 갱신된다. 사용자가 실시간으로 정보를 표시하지 않는 볼 그래프. 폴더를 확대하고 이전 폴더로 돌아갑니다 .. CD를 클릭하여 그래프의 영역을 클릭합니다.';
+$lang['disk_usage_coordinate_invalid'] = '좌표가 잘못되었습니다.';
+$lang['disk_usage_path_invalid'] = '경로가 잘못되었습니다.';
+$lang['disk_usage_updating_disk_usage_information'] = '디스크 사용 정보를 업데이트. 이 작업은 몇 분 정도 걸릴 수 있습니다.';
