c2d6e05d892d9d660b1289721b80ee92e00a9a6a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:36:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'The lebili tšebeliso Report app e bontša hao tsamaiso disekethata tšebeliso le fana ka clickable go tirila fatše tlaleho ena.';
+$lang['disk_usage_app_name'] = 'Tiske tšebeliso Report';
+$lang['disk_usage_app_tooltip'] = 'Tiske tšebeliso tlhahisoleseding e bolokoa k&#39;homphieutheng bakeng itima lijo mofuteng le ntjhafatswa mong le e mong lihora tse 24. Kerafo o sheba ha bontša sebele ea nako boitsebiso bo eketsehileng. Tobetsa libakeng tse sa kerafo ho Nyenyefatsa ka difoldara le tobetsa Cd .. ho khutlela foldara e fetileng.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-golaganya ha o sebetse.';
+$lang['disk_usage_path_invalid'] = 'Path ha o sebetse.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Go tlhabololwa fumane boitsebiso bo tšebeliso tiske. Sena se ka nka metsotso e &#39;maloa.';
