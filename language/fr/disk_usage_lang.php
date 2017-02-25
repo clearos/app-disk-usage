cffdff2948ba35a6a10691a082708ddef2af167b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:30:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'L&#39;utilisation du disque Rapport application affiche votre système utilisation du disque dur et fournit un drill down rapport cliquable.';
+$lang['disk_usage_app_name'] = 'Utilisation du disque Rapport';
+$lang['disk_usage_app_tooltip'] = 'informations sur l&#39;utilisation du disque est stocké dans une base de données pour une récupération rapide et mis à jour toutes les 24 heures. Le graphique que vous voyez ne pas afficher des informations en temps réel. Cliquez sur les zones du graphe pour zoomer dans des dossiers et cliquez sur cd .. pour revenir au dossier précédent.';
+$lang['disk_usage_coordinate_invalid'] = 'Coordonner est invalide.';
+$lang['disk_usage_path_invalid'] = 'Path est invalide.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Mise à jour des informations d&#39;utilisation du disque. Cela peut prendre plusieurs minutes.';
