f65fd807da4ded6ea0919766556d50791bea3453
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:31:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'A Lemezhasználat Report alkalmazás megjeleníti a rendszer merevlemez használatát, és egy kattintható lebontja jelentést.';
+$lang['disk_usage_app_name'] = 'Lemezhasználat jelentés';
+$lang['disk_usage_app_tooltip'] = 'Lemezhasználati információkat egy adatbázisban tárolják a gyors visszakeresés és 24 óránként frissülnek. A grafikon megtekintésekor nem jelenik meg a valós idejű információkat. Kattints a területeken a grafikon nagyítható mappák és kattintson a cd .. visszatérhet az előző képernyőre.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordináta érvénytelen.';
+$lang['disk_usage_path_invalid'] = 'Path érvénytelen.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Frissítése lemez használati információk. Ez több percig is eltarthat.';
