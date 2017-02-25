1d4fe8492c916a7a15ae06ae0947d303540dfed6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:34:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'De Disk Usage Report app toont uw gebruik van het systeem harde schijf en biedt een klikbare drill down rapport.';
+$lang['disk_usage_app_name'] = 'Schijfgebruik Report';
+$lang['disk_usage_app_tooltip'] = 'Schijfgebruik zijn opgeslagen in een database voor het snel opzoeken en wordt elke 24 uur. De grafiek die u bekijkt niet real-time informatie te geven. Klik op het gebied van de grafiek in mappen te zoomen en klik op cd .. om terug te keren naar de vorige map.';
+$lang['disk_usage_coordinate_invalid'] = 'Coördineren is ongeldig.';
+$lang['disk_usage_path_invalid'] = 'Path is ongeldig.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Het bijwerken van schijfgebruik informatie. Dit kan enkele minuten duren.';
