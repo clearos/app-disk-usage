1c28c7b802bb786cc2be6de09a57033edb8fdae2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:37:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Diskanvändningsrapport app visar systemet hårddisk användning och ger en klickbar borra ner rapport.';
+$lang['disk_usage_app_name'] = 'Diskanvändning Rapport';
+$lang['disk_usage_app_tooltip'] = 'Diskanvändning information lagras i en databas för snabb hämtning och uppdateras var 24: e timme. Grafen du tittar inte visa realtidsinformation. Klicka på de områden av diagrammet att zooma in mappar och klicka på cd .. för att återgå till föregående mapp.';
+$lang['disk_usage_coordinate_invalid'] = 'Samordna är ogiltig.';
+$lang['disk_usage_path_invalid'] = 'Path är ogiltig.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Uppdatering diskanvändningsinformation. Detta kan ta flera minuter.';
