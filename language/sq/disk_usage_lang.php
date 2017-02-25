7ffa0508cafd6723392a8d3c6b2ebdc887109d7d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:27:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disk Usage Raporti app tregon sistemin përdorimin tuaj hard disk dhe të siguron një stërvitje poshtë raportin e klikueshme.';
+$lang['disk_usage_app_name'] = 'Disk Usage Raporti';
+$lang['disk_usage_app_tooltip'] = 'Informacione Përdorimi Disk ruhet në një bazë të dhënash për rikthim të shpejtë dhe të përditësuar çdo 24 orë. Grafiku jeni duke parë nuk të shfaqur informacion në kohë reale. Kliko në fushat e grafikut të zoom në dosje dhe klikoni mbi cd .. për t&#39;u kthyer në dosje të mëparshme.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinojë është e pavlefshme.';
+$lang['disk_usage_path_invalid'] = 'Rruga është e pavlefshme.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Përditësimin e informacionit të përdorimit disk. Kjo mund të zgjasë disa minuta.';
