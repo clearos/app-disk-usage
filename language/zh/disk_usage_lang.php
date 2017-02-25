fead226262e5fc4011bf7b0a846e0f8205400f27
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:29:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = '磁盘使用情况报告的应用程序会显示您的系统硬盘的使用情况，并提供了一个可点击的向下钻取报告。';
+$lang['disk_usage_app_name'] = '磁盘使用情况报告';
+$lang['disk_usage_app_tooltip'] = '磁盘使用信息存储在用于快速检索数据库和更新，每24小时。 您正在查看不显示实时信息的图表。 点击图的区域放大到文件夹，然后点击CD ..返回到前一个文件夹。';
+$lang['disk_usage_coordinate_invalid'] = '坐标是无效的。';
+$lang['disk_usage_path_invalid'] = '路径无效。';
+$lang['disk_usage_updating_disk_usage_information'] = '更新磁盘使用情况的信息。 这可能需要几分钟的时间。';
