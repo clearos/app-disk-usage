85b165b49a7bd932802fe19427d8d08bf5c79068
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:34:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Il-app Disk Użu Rapport displays sistema użu tiegħek hard disk u jipprovdi drill stabbiliti rapport clickable.';
+$lang['disk_usage_app_name'] = 'Disk Użu Rapport';
+$lang['disk_usage_app_tooltip'] = 'Informazzjoni dwar lużu disk hi maħżuna f&#39;database għall-irkupru mgħaġġel u aġġornata kull 24 siegħa. Il-graff inti viewing ma juri informazzjoni f&#39;ħin reali. Ikklikkja fuq l-oqsma ta &#39;l-graff li zoom fil-folders u kklikkja fuq cd .. li jirritornaw għall-folder preċedenti.';
+$lang['disk_usage_coordinate_invalid'] = 'Ko-ordinati huwa invalidu.';
+$lang['disk_usage_path_invalid'] = 'Path huwa invalidu.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Aġġornar tat-tagħrif użu disk. Dan jista &#39;jieħu diversi minuti.';
