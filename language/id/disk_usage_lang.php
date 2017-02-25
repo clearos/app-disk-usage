b364d8c2c008978931a9da379701317d0d33789f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:31:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disk Usage Report aplikasi menampilkan penggunaan hard disk sistem anda dan memberikan menelusuri laporan dapat diklik.';
+$lang['disk_usage_app_name'] = 'Disk Usage Report';
+$lang['disk_usage_app_tooltip'] = 'informasi penggunaan disk disimpan dalam database untuk pencarian cepat dan diperbarui setiap 24 jam. Grafik yang Anda lihat tidak menampilkan informasi real-time. Klik pada bidang grafik untuk memperbesar folder dan klik pada cd .. untuk kembali ke folder sebelumnya.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinasi tidak valid.';
+$lang['disk_usage_path_invalid'] = 'Jalan tidak valid.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Memperbarui informasi penggunaan disk. Hal ini dapat berlangsung beberapa menit.';
