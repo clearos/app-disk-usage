dba1f6f2430f2d71104b13cd012a23362e9d36fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:27:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disk istifadə Report app sistem sabit disk istifadə göstərir və tıklanabilir aşağı qazmaq hesabat verir.';
+$lang['disk_usage_app_name'] = 'Disk istifadə Report';
+$lang['disk_usage_app_tooltip'] = 'Disk istifadə məlumat hər 24 saat sürətli alınması üçün məlumat bazasında saxlanılır və yenilənir. Siz real-time məlumat keçirmək deyil baxırsınız graph. qovluq daxil zoom və əvvəlki qovluğa qayıtmaq .. cd basın graph sahələri basın.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinat yanlışdır.';
+$lang['disk_usage_path_invalid'] = 'Path yanlışdır.';
+$lang['disk_usage_updating_disk_usage_information'] = 'disk istifadə məlumat yenilənməsi. Bu bir neçə dəqiqə çəkə bilər.';
