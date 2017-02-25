ecace16b078d37ca89a8c3f84ccdbf004a9f389b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:38:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disk Foydalanish hisoboti ilova tizim qattiq disk foydalanishni ko&#39;rsatadi va bir tıklanabilir detaya hisobot beradi.';
+$lang['disk_usage_app_name'] = 'Disk Foydalanish hisoboti';
+$lang['disk_usage_app_tooltip'] = 'Disk foydalanish haqida ma&#39;lumot har 24 soatda tez qabul qilish uchun ma&#39;lumotlar bazasida saqlanadi va yangilanadi. Agar real vaqt ma&#39;lumotlarni aks emas kuzatishmoqda graph. papkalar ichiga yaqinlashtirish va oldingi papkaga qaytish uchun .. CD bosing grafik sohalarda bosing.';
+$lang['disk_usage_coordinate_invalid'] = 'Muvofiq bekor.';
+$lang['disk_usage_path_invalid'] = 'Yo&#39;l bekor.';
+$lang['disk_usage_updating_disk_usage_information'] = 'disk foydalanish ma&#39;lumotlarini yangilash. Bu bir necha daqiqa o&#39;tishi mumkin.';
