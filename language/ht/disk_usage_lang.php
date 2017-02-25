97ac146e6ac73f0be7543130e4c520b38815db1f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:30:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'App a Disk Itilizasyon Rapò montre sistèm l &#39;ki gen kapasite difisil ou epi li bay yon klikabl fè egzèsis desann rapò.';
+$lang['disk_usage_app_name'] = 'Disk Itilizasyon Rapò';
+$lang['disk_usage_app_tooltip'] = 'se enfòmasyon l &#39;Disk estoke nan yon baz done pou Rekipere vit ak mete ajou chak 24 èdtan. graf la ou se gade pa montre enfòmasyon an tan reyèl. Klike sou zòn nan graf la rale nan dosye, epi klike sou CD .. pou li retounen nan folder nan anvan yo.';
+$lang['disk_usage_coordinate_invalid'] = 'Ko-ordinate se valab.';
+$lang['disk_usage_path_invalid'] = 'Path se valab.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Mete ajou enfòmasyon itilizasyon ki gen kapasite. Sa a ka pran plizyè minit.';
