5565708f51fdcae4e128a81928aa327d18fa98fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:29:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'La Disk Uzado Raporti app montras vian sistemon malmola disko uzado kaj provizas alklakebla bori malsupren raporto.';
+$lang['disk_usage_app_name'] = 'Disk Uzado Raporti';
+$lang['disk_usage_app_tooltip'] = 'Disko uzado informoj estas stokitaj en datumbazo por rapida rehavigo kaj ĝisdatigita je ĉiuj 24 horoj. La grafikaĵo vi vidas ne montras informon en reala tempo. Klaku sur la areoj de la grafeo zomi en dosierujoj kaj alklaku cd .. reveni al la antaŭa dosierujo.';
+$lang['disk_usage_coordinate_invalid'] = 'Kunordigi estas nevalida.';
+$lang['disk_usage_path_invalid'] = 'Vojo estas nevalida.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Ĝisdatiganta disko uzado informo. Tio povas preni plurajn minutojn.';
