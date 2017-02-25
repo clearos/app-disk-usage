9a6f8447bbd58e6f808663081858bc595256dd27
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:35:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Raport Disk Utilizare Aplicația afișează utilizarea sistemului de disc și oferă un raport detalia clickable.';
+$lang['disk_usage_app_name'] = 'Raport de disc Utilizare';
+$lang['disk_usage_app_tooltip'] = 'Informații de utilizare a discului sunt stocate într-o bază de date pentru recuperare rapidă și actualizată la fiecare 24 de ore. Grafic pe care îl vizualizați nu afișează informații în timp real. Faceți clic pe zonele din grafic pentru a mări în foldere și faceți clic pe cd .. pentru a reveni la directorul anterior.';
+$lang['disk_usage_coordinate_invalid'] = 'Coordonate nu este validă.';
+$lang['disk_usage_path_invalid'] = 'Cale este nevalid.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Actualizarea informațiilor referitoare la utilizarea discului. Acest lucru poate dura câteva minute.';
