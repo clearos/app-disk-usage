95a0405f2c1e8feb2ce5af04807007947ec56e3a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:31:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Taispeánann an app Tuairisc Úsáid Diosca do chóras úsáid diosca crua agus soláthraíonn chliceáil druil síos tuarascáil.';
+$lang['disk_usage_app_name'] = 'Tuairisc Úsáid Diosca';
+$lang['disk_usage_app_tooltip'] = 'Tá eolas diosca stóráil i mbunachar sonraí le haghaidh aisghabhála tapa agus cothrom le dáta gach 24 uair an chloig. An graf tú ag amharc ní íomhá eolas fíor-ama. Cliceáil ar na réimsí den ghraf a zúmáil isteach fillteáin agus cliceáil ar CD .. chun filleadh ar an fillteán roimhe sin.';
+$lang['disk_usage_coordinate_invalid'] = 'Is Comhordú neamhbhailí.';
+$lang['disk_usage_path_invalid'] = 'Is Conair neamhbhailí.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Thabhairt cothrom le dáta eolas diosca. Is féidir é seo a ghlacadh roinnt nóiméad.';
