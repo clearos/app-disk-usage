eaa176a93f4c528243259df6d52b5f4ce6627a63
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:32:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Orbis Ritus Report App ostendit vobis, et usum praebet clickable EXERCITATIO difficile orbis ratio fama.';
+$lang['disk_usage_app_name'] = 'Orbis Ritus Report';
+$lang['disk_usage_app_tooltip'] = 'Orbis usu ieiunium notitia retrieval et repono database renovatur in XXIV horis. Propono tibi visum non est verus-vicis notitia purus. Click to zoom in campo purus folder quod click in priore ad CD .. folder.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-ordinate, invalidum est.';
+$lang['disk_usage_path_invalid'] = 'Semita est invalidum.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Adaequationis orbis mos notitia. Hoc potest ad plures minutes.';
