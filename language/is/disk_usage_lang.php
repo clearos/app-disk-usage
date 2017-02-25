70a4aa654daae42af42ef3e236fce0a8501376e4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:31:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'The Diskur Notkun Report app birtir kerfið harður diskur notkun og veitir smella á bora niður skýrslu.';
+$lang['disk_usage_app_name'] = 'Diskur Notkun Report';
+$lang['disk_usage_app_tooltip'] = 'Disknotkun upplýsingar eru geymdar í gagnagrunni fyrir fljótur sókn og uppfærð á 24 klukkustunda fresti. Grafið þú ert að skoða er ekki birt upplýsingar í rauntíma. Smelltu á sviði línurit til að stækka í möppur og smelltu á cd .. til að fara aftur í fyrri möppu.';
+$lang['disk_usage_coordinate_invalid'] = 'Samræma er ógild.';
+$lang['disk_usage_path_invalid'] = 'Slóðin er ógilt.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Uppfærsla diskur notkun upplýsinga. Þetta getur tekið nokkrar mínútur.';
