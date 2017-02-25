185b01c5dbdcdd1d1f3bfe2fa09500e37c1b2c15
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:32:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'ディスク使用状況のレポートのアプリは、あなたのシステムのハード・ディスクの使用状況が表示され、クリック可能なドリルダウンレポートを提供します。';
+$lang['disk_usage_app_name'] = 'ディスク使用状況のレポート';
+$lang['disk_usage_app_tooltip'] = 'ディスク使用状況情報は、高速検索のためにデータベースに格納され、24時間ごとに更新されます。 表示しているグラフはリアルタイムの情報が表示されません。 フォルダにズームインし、前のフォルダに戻るに.. CDをクリックして、グラフの領域をクリックしてください。';
+$lang['disk_usage_coordinate_invalid'] = '座標が無効です。';
+$lang['disk_usage_path_invalid'] = 'パスが無効です。';
+$lang['disk_usage_updating_disk_usage_information'] = 'ディスクの使用状況の情報を更新します。 これには数分かかることがあります。';
