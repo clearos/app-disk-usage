dfb29cbae68a2fab89b9cbda7381f9c2a1bc47da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:36:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disk Matumizi Ripoti programu maonyesho mfumo wako hard disk matumizi na hutoa clickable kuchimba chini ya ripoti hiyo.';
+$lang['disk_usage_app_name'] = 'Disk Matumizi Ripoti';
+$lang['disk_usage_app_tooltip'] = 'Disk matumizi habari ni kuhifadhiwa katika database kwa haraka retrieval na updated kila masaa 24. graph wewe ni kuangalia haina kuonyesha halisi wakati habari. Click kwenye maeneo ya graph kwa kuvuta katika folders na bonyeza cd .. kurudi folder uliopita.';
+$lang['disk_usage_coordinate_invalid'] = 'Kuratibu ni batili.';
+$lang['disk_usage_path_invalid'] = 'Njia ni batili.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Uppdatering habari matumizi disk. Hii inaweza kuchukua dakika kadhaa.';
