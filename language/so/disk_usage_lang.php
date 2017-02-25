19b425002a8fbaf96c45786101a5d6cab4189171
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:36:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'app Report Isticmaalka digsi soo bandhigayaa hab aad isticmaalka disk adag iyo bixisaa clickable warbixin qodo hoos.';
+$lang['disk_usage_app_name'] = 'Report Isticmaalka digsi';
+$lang['disk_usage_app_tooltip'] = 'macluumaad isticmaalka digsi waxa lagu kaydiyaa database a for wax kaqabashada degdeg ah oo updated 24 saac kasta. Shaxda aad arkaysid ma muujiyaan macluumaadka-waqtiga dhabta ah. Riix meelaha garaafka Mozilla galay fayl oo guji on cd .. inuu ku laabto folder hore.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-duwo waa baadil.';
+$lang['disk_usage_path_invalid'] = 'Jidka waa baadil.';
+$lang['disk_usage_updating_disk_usage_information'] = 'CUSBOONAYSIINTA macluumaad isticmaalka disk. Tani waxay qaadan kartaa dhowr daqiiqo.';
