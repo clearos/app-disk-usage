50204efbd0b51272a1a73bb053a6885af6d13394
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:27:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Die skyf Gebruik Verslag app gee jou stelsel gebruik hardeskyf en bied &#39;n klikbare boor af verslag.';
+$lang['disk_usage_app_name'] = 'Disk Usage Verslag';
+$lang['disk_usage_app_tooltip'] = 'Skyf gebruik inligting word gestoor in &#39;n databasis vir &#39;n vinnige herwinning en opgedateer elke 24 uur. Die grafiek wat jy besoek nie real-time inligting te vertoon. Klik op die gebiede van die grafiek in dopgehou om te vergroot en klik op CD .. om terug te keer na die vorige gids.';
+$lang['disk_usage_coordinate_invalid'] = 'Koördineer is ongeldig.';
+$lang['disk_usage_path_invalid'] = 'Pad is ongeldig.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Opdatering skyf gebruik inligting. Dit kan etlike minute duur.';
