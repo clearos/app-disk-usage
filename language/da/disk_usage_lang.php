8492bbe7ef8ea6ac966f1589e5cc8f318eef5d5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:29:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disk Usage Report app viser dit system hard diskforbrug og giver et klikbart bore ned rapport.';
+$lang['disk_usage_app_name'] = 'Disk Usage Report';
+$lang['disk_usage_app_tooltip'] = 'Disk forbrug oplysninger lagres i en database til hurtig hentning og opdateres hver 24 timer. Grafen du ser ikke viser real-time information. Klik på de områder af grafen for at zoome i mapper og klik på cd .. for at vende tilbage til den forrige mappe.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinere er ugyldig.';
+$lang['disk_usage_path_invalid'] = 'Path er ugyldig.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Opdatering diskforbrug oplysninger. Dette kan tage flere minutter.';
