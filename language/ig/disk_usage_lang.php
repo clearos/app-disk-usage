66c1b137f233403558ad58fb8f44b34844382685
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:31:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'The diski ojiji Report ngwa na-egosiputa gị usoro diski ike ojiji na-enye a clickable akuakuzinwo ala akụkọ.';
+$lang['disk_usage_app_name'] = 'Disk ojiji Report';
+$lang['disk_usage_app_tooltip'] = 'Disk ojiji ozi na-echekwara na a nchekwa data maka ngwa ngwa nlaghachie na-emelite kwa 24 awa. The eserese ị na-elele adịghị egosipụta ezigbo oge ozi. Pịa na ebe nke eserese ka ibubaa n&#39;ime nchekwa na pịa cd .. laghachi aga na nchekwa.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-ordinate bụ ghara ịdị irè.';
+$lang['disk_usage_path_invalid'] = 'Ụzọ bụ ghara ịdị irè.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Emelite disk ojiji ozi. Nke a nwere ike na-ọtụtụ minit.';
