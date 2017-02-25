016b4825e8dcdb7b03711c449ff6cc98abe5e2c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:29:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Zpráva o využití disku aplikace zobrazuje systémové využití pevného disku a poskytuje drill-down zprávy klikací.';
+$lang['disk_usage_app_name'] = 'Zpráva o využití disku';
+$lang['disk_usage_app_tooltip'] = 'Informace o využití disku jsou uloženy v databázi pro rychlé vyhledávání a aktualizují každých 24 hodin. Z grafu je prohlížíte nezobrazuje informace v reálném čase. Kliknutím na oblasti grafu pro přiblížení do složek a klikněte na cd .. Pro návrat do předchozí složky.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordinace je neplatný.';
+$lang['disk_usage_path_invalid'] = 'Cesta je neplatná.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Aktualizace informace o využití disku. To může trvat několik minut.';
