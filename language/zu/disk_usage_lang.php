ee958f57da1c3d5bbc5ff52c184ebce037457110
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:39:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Insiza Umbiko Ukusetshenziswa kwediski ubonisa ohlelweni lwakho hard disk ukusetshenziswa futhi inikeza icindezeleka sokuprakthiza phansi umbiko.';
+$lang['disk_usage_app_name'] = 'Umbiko Ukusetshenziswa kwediski';
+$lang['disk_usage_app_tooltip'] = 'Imininingwane Disk ukusetshenziswa lilondolozwe database for ukubuyisa fast and olusha njalo ngemva kwamahora angu-24. Igrafu ubheka akusho ukubonisa ukwaziswa real-time. Chofoza ezindaweni igrafu ukusondeza kumafolda bese uchofoze on cd .. ukubuyela kufolda odlule.';
+$lang['disk_usage_coordinate_invalid'] = 'Ukuqondanisa ayivumelekile.';
+$lang['disk_usage_path_invalid'] = 'Path ayivumelekile.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Ubuyekeza imininingwane disk ukusetshenziswa. Lokhu kungathatha amaminithi ambalwa.';
