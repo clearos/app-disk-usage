840a10919614be3028beb55d12437ab5c03a9754
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:36:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Nu piringan Dianggo Laporan app mintonkeun pamakean disk teuas sistim Anjeun jeung nyadiakeun bor turun laporan clickable.';
+$lang['disk_usage_app_name'] = 'Piringan Dianggo Laporan';
+$lang['disk_usage_app_tooltip'] = 'informasi pamakéan piringan disimpen dina database pikeun dimeunangkeun saum sareng diropéa unggal 24 jam. Nu grafik Anjeun keur nempo teu mintonkeun informasi real-time. Klik dina wewengkon grafik pikeun ngazum gede kana polder jeung klik dina cd .. balik ka polder saméméhna.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-ordinate téh sah.';
+$lang['disk_usage_path_invalid'] = 'Path téh sah.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Ngamutahirkeun informasi pamakéan disk. Hal ieu bisa nyokot sababaraha menit.';
