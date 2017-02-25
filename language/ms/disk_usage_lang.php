99890f37236b6053c3621316bc80bad1f87faf77
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:34:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Laporan Disk Penggunaan aplikasi memaparkan penggunaan cakera keras sistem anda dan menyediakan mengesan laporan yang boleh diklik.';
+$lang['disk_usage_app_name'] = 'Laporan Disk Usage';
+$lang['disk_usage_app_tooltip'] = 'maklumat penggunaan Disk disimpan di dalam pangkalan data untuk mendapatkan semula cepat dan dikemas kini setiap 24 jam. Graf anda melihat tidak memaparkan maklumat masa nyata. Klik pada bidang graf untuk zum ke dalam folder dan klik pada cd .. untuk kembali ke folder sebelumnya.';
+$lang['disk_usage_coordinate_invalid'] = 'Menyelaras tidak sah.';
+$lang['disk_usage_path_invalid'] = 'Jalan tidak sah.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Mengemaskini maklumat penggunaan cakera. Ini boleh mengambil masa beberapa minit.';
