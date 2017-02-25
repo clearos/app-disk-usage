48b5ece6b7afa93e21b27882630274269eadd987
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:35:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Raport Użycie dysku aplikacja wyświetla systemu Wykorzystanie dysku twardego i zapewnia drążyć raport klikalny.';
+$lang['disk_usage_app_name'] = 'Raport Użycie dysku';
+$lang['disk_usage_app_tooltip'] = 'informacje o wykorzystaniu dysku są przechowywane w bazie danych dla szybkiego pobierania i aktualizowane co 24 godziny. Wykres oglądania nie wyświetla informacji w czasie rzeczywistym. Kliknij na obszarze wykresu, aby powiększyć w folderach i kliknij na cd .., aby powrócić do poprzedniego folderu.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordynacja jest nieprawidłowy.';
+$lang['disk_usage_path_invalid'] = 'Ścieżka jest nieprawidłowy.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Aktualizacja informacji o użytkowaniu dysków. Może to potrwać kilka minut.';
