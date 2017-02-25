f2340ea3451635bdc55599c414fa05261582a1df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:29:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Disk Usage Report rakendus kuvab süsteemi kõvakettale kasutamist ja annab klikitav süvitsiminemis- aruande.';
+$lang['disk_usage_app_name'] = 'Disk Usage aruanne';
+$lang['disk_usage_app_tooltip'] = 'Kettakasutust andmed salvestatakse andmebaasi kiire ülekanne ja uuendatakse iga 24 tunni jooksul. Graafik sa vaatad ei näidata reaalajas teavet. Vajuta valdkondades graafik suurendamiseks kaustadesse ja kliki cd .. naasta eelmisse kausta.';
+$lang['disk_usage_coordinate_invalid'] = 'Koordineerida on kehtetu.';
+$lang['disk_usage_path_invalid'] = 'Tee on kehtetu.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Uuendamine ketta kasutamise kohta. Selleks võib kuluda mitu minutit.';
