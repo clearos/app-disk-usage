6ed4809d6302e46841e1c4e27efe02b7b36e23f8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:28:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/disk_usage_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['disk_usage_app_description'] = 'Litayamba Kagwiritsidwe Report app amasonyeza dongosolo mwakhama litayamba wakagwiritsidwe ndipo amapereka clickable kubowola pansi lipoti.';
+$lang['disk_usage_app_name'] = 'Litayamba Kagwiritsidwe Report';
+$lang['disk_usage_app_tooltip'] = 'Litayamba wakagwiritsidwe zambiri amasungidwa Nawonso achichepere kwa kudya katengedwe ndi kusinthidwa mawola 24. The Zithunzi inu kuonera si kusonyeza zenizeni nthawi zambiri. Dinani malo a Zithunzi kuti mawonedwe mu mafoda ndipo alemba pa CD .. kubwerera chikwatu m&#39;mbuyomu.';
+$lang['disk_usage_coordinate_invalid'] = 'Co-kukhazikitsa ndi opanda pake.';
+$lang['disk_usage_path_invalid'] = 'Njira ili udzakhala osazindikilika.';
+$lang['disk_usage_updating_disk_usage_information'] = 'Kasinthidwe mudziwe wakagwiritsidwe litayamba. Izi zikhoza kutenga mphindi zingapo.';
